0bb7e954429fb3547a2c12a7598d57eaa0d63d6d
sherrytherese <sherry.therese@gmail.com>
2016-03-06 20:12:45 -0800

    Added jquery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
-	${'ItemEntry').val()}
-})
+
+$(document).ready(function() {
+		var addItem = $('.additem');
+
+	addItem.click(function() {
+		var toAdd = $('.TextInput').val();
+		$('.ShoppingList').append('<li><input type = "checkbox">' + toAdd +'<button class = "remove">Remove Item </button></li>');
+	});
+
+	$('.ShoppingList').on('click', '.remove', function(){
+		$(this).parent("li").remove();
+	});
+
+
+
+	//$('.ButtonAdd').click(function() {
+	//	listitemtext = $('.TextInput').val();
+	//	listitem = $('<li class = "item">').text(listitemtext);
+	//	console.log(listitem);
+	//$('.List').append(listitem);
+	//})
+});	
